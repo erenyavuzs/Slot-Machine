c82f47378fbd0b4c463ffb453c3a3fc88febb9ee
Eren Yavuz <109018781+erenyavuzs@users.noreply.github.com>
2024-08-26 14:41:16 +0300

    WIP

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
-MAX_LİNES = 3
+import random
+
+MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
+ROWS = 3
+COLS = 3
+
+symbol_count= {
+    "7": 2,
+    "BAR": 4,
+    "cherries": 6,
+    "lemon": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    colums = []
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        colums.append(column)
+
+    return colums
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
+            
+            
+
+            
 def deposit():
     while True:
         amount = input("\nWhat would you like to deposit? $")
@@ -19,10 +61,10 @@ def deposit():
 
 def get_number_of_lines():
     while True:
-        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LİNES) + ")? ")
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
         if lines.isdigit():
             lines = int(lines)
-            if 1 <= lines <= MAX_LİNES:
+            if 1 <= lines <= MAX_LINES:
                 break
             else:
                 print("Enter a valid number of lines !")
